e177210be6022d8351244108a459ec86a38b68f7
Ben Cradick <ben.cradick@gmail.com>
2018-09-24 19:01:10 -0500

    Parts A->D done, testing part C for better iterative data

---
+++ CRAB1D/CRAB1D.py
@@ -0,0 +1,51 @@
+class BigIntFibonacci:
+    def __init__(self):
+        self.previous = [0 for x in range(0,99)]
+        self.penultimate = [0 for x in range(0,99)]
+        self.current = [0 for x in range(0,100)]
+        self.previous[0] = 1
+        self.current[0] = 1
+        self.N = 0
+
+    def addTwoCells(self,location):
+        if self.previous[location] + self.penultimate[location] + self.current[location] >= 10:
+            self.current[location + 1] += 1
+            temp = self.current[location] + self.previous[location] + self.penultimate[location]
+            temp %= 10
+            self.current[location] = temp
+        else:
+            self.current[location] += self.previous[location]
+            self.current[location] += self.penultimate[location]
+    
+    def reset(self):
+        self.penultimate = [0 for x in range(0,100)]
+        self.penultimate = self.previous.copy()
+        self.previous = [0 for x in range(0,100)]
+        self.previous = self.current.copy()
+        self.current = [0 for x in range(0,100)]
+
+    def calcCurrent(self):
+        for loc in range(0,99):
+            self.addTwoCells(loc)
+
+    def findMax99digitFib(self):
+        while self.current[99] == 0:
+            self.reset()
+            self.calcCurrent()
+        self.printBigInt()
+    def printBigInt(self):
+        result = open("CRAB1D/results.txt", "w")
+        formatted = " "
+        for x in range(0,99):
+            if x%3 == 0:
+                formatted+=","
+            formatted+=str(self.previous[99-x])
+        
+        print(f"{formatted[3:len(formatted)]}", file=result)
+FibNums = BigIntFibonacci()
+FibNums.findMax99digitFib()
+
+    
+
+
+    
